bafc2c5d3433556a2023d165d0108bf0b40172a2
saj panchal <sajpanchal2020@outlook.com>
2021-10-05 10:56:58 -0400

    project initiated.

---
+++ BarcodeScanne/BarcodeScanneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScanneApp.swift
+//  BarcodeScanne
+//
+//  Created by saj panchal on 2021-10-05.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScanneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
